90ed394d00c8a2ea8caa761cb201ed3eca5148e9
Quang Ho <hoqm01@pfw.edu>
2026-03-17 23:27:44 -0400

    images 3

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -5,13 +5,16 @@ import InfiniteGallery from './components/ui/infinite-gallery';
 
 const galleryImages = [
   "https://i.postimg.cc/B6cnr3g4/Quang-Ho-Volunteer.png",
-  "https://i.postimg.cc/9f23v7Nb/z6567340031129-657d7b543f5bd01c478b636da7ec8459.jpg",
+  "https://i.postimg.cc/RFsZSRkC/IMG-5457.jpg",
+
   "https://i.postimg.cc/KcH6mVH1/z6567340289869-12a36686c7a3e6aae8c6b5f83092aa1a.jpg",
+
+  "https://i.postimg.cc/QNYFh8DB/IMG-5449.jpg",
+  "https://i.postimg.cc/9f23v7Nb/z6567340031129-657d7b543f5bd01c478b636da7ec8459.jpg",
+  "https://i.postimg.cc/sDt1GT1L/IMG-5453.jpg",
+  "https://i.postimg.cc/R0mFXq7s/IMG-5455.jpg",
   "https://i.postimg.cc/htBcq6X4/z6567357736285-31966aaa2732068a2554ff5a7258938b.jpg",
-  "https://picsum.photos/seed/teamwork5/800/600",
-  "https://picsum.photos/seed/education6/800/600",
-  "https://picsum.photos/seed/workshop7/800/600",
-  "https://picsum.photos/seed/project8/800/600",
+  "https://i.postimg.cc/zDQG4xw0/IMG-5458.jpg"
 ];
 
 export default function LandingPage() {
